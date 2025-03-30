f0ec58d888eb5ea88e2fbbd925ad803df6ad2899
Luís Fernando Sampaio <lufesampaio@gmail.com>
2025-03-29 20:13:16 -0400

    feat: estrutura inicial do projeto

---
+++ src/main/java/com/seplag/processoseletivo/ProcessoseletivoApplication.java
@@ -0,0 +1,13 @@
+package com.seplag.processoseletivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessoseletivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessoseletivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seplag/processoseletivo/ProcessoseletivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seplag.processoseletivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessoseletivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
