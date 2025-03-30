8e589befde9f917209d8d8bfe4272364d5b5f2e6
Luís Fernando Sampaio <lufesampaio@gmail.com>
2025-03-30 11:42:21 -0400

    chore: bean config

--- src/main/java/com/seplag/processoseletivo/infra/config/ApplicationConfig.java
+++ src/main/java/com/seplag/processoseletivo/infra/config/ApplicationConfig.java
@@ -1,8 +1,11 @@
 package com.seplag.processoseletivo.infra.config;
 
 
+import com.seplag.processoseletivo.domain.repositories.CidadeRepository;
 import com.seplag.processoseletivo.domain.repositories.UnidadeRepository;
+import com.seplag.processoseletivo.infra.persistence.repositories.CidadeRepositoryImpl;
 import com.seplag.processoseletivo.infra.persistence.repositories.UnidadeRepositoryImpl;
+import com.seplag.processoseletivo.infra.persistence.repositories.jpa.CidadeJpaRepository;
 import com.seplag.processoseletivo.infra.persistence.repositories.jpa.UnidadeJpaRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +16,11 @@ public class ApplicationConfig {
     @Bean
     public UnidadeRepository unidadeRepository(UnidadeJpaRepository unidadeJpaRepository) {
         return new UnidadeRepositoryImpl(unidadeJpaRepository);
+    }
+
+    @Bean
+    public CidadeRepository cidadeRepository(CidadeJpaRepository cidadeJpaRepository) {
+        return new CidadeRepositoryImpl(cidadeJpaRepository);
     }
 
 }
