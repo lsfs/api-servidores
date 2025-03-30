6099c2b315040f3255024366f74113664988b92d
Luís Fernando Sampaio <lufesampaio@gmail.com>
2025-03-30 15:43:34 -0400

    fix: ajustes nos mappers e entidade de unidade

--- src/main/java/com/seplag/processoseletivo/infra/mapper/UnidadeMapper.java
+++ src/main/java/com/seplag/processoseletivo/infra/mapper/UnidadeMapper.java
@@ -3,6 +3,8 @@ package com.seplag.processoseletivo.infra.mapper;
 import com.seplag.processoseletivo.domain.model.Unidade;
 import com.seplag.processoseletivo.infra.persistence.entity.UnidadeEntity;
 
+import java.util.stream.Collectors;
+
 public class UnidadeMapper {
 
 
@@ -12,8 +14,25 @@ public class UnidadeMapper {
         unidade.setUnid_id(entity.getUnid_id());
         unidade.setUnid_nome(entity.getUnid_nome());
         unidade.setUni_sigla(entity.getUnid_sigla());
+        unidade.setEnderecos(
+                entity.getEnderecos()
+                        .stream()
+                        .map(EnderecoMapper::toModel)
+                        .collect(Collectors.toSet())
+        );
 
         return unidade;
+
+    }
+
+    public static UnidadeEntity toEntity(Unidade unidade) {
+
+        UnidadeEntity unidadeEntity = new UnidadeEntity();
+        unidadeEntity.setUnid_id(unidade.getUnid_id());
+        unidadeEntity.setUnid_nome(unidade.getUnid_nome());
+        unidadeEntity.setUnid_sigla(unidade.getUni_sigla());
+
+        return unidadeEntity;
 
     }
 
