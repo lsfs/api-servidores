c3e9ed50add6d88d70b8990962aae730f5a92a4d
Luís Fernando Sampaio <lufesampaio@gmail.com>
2025-03-30 22:51:42 -0400

    fix: ajustes nas classes de dominio

--- src/main/java/com/seplag/processoseletivo/domain/repositories/ServidorTemporarioRepository.java
+++ src/main/java/com/seplag/processoseletivo/domain/repositories/ServidorTemporarioRepository.java
@@ -1,5 +1,6 @@
 package com.seplag.processoseletivo.domain.repositories;
 
+import com.seplag.processoseletivo.application.dto.shared.MensagemRetorno;
 import com.seplag.processoseletivo.domain.model.ServidorTemporario;
 import com.seplag.processoseletivo.domain.utils.RespostaPaginada;
 
@@ -8,6 +9,7 @@ public interface ServidorTemporarioRepository {
     ServidorTemporario criar(ServidorTemporario servidorTemporario);
     ServidorTemporario buscarPorId(Long id);
     RespostaPaginada<ServidorTemporario> buscaServidoresTemporarios(int pagina, int tamanho);
-    ServidorTemporario atualizar(ServidorTemporario servidorTemporario);
+    ServidorTemporario atualizar(Long id, ServidorTemporario servidorTemporario);
+    void deletar(Long id);
 
 }
