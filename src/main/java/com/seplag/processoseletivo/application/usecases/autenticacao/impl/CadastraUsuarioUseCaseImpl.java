b7b46d2043f625bde1abbe18f9675e3262c352c4
Luís Fernando Sampaio <lufesampaio@gmail.com>
2025-04-09 01:25:45 -0400

    feat: salva usuario como admin

--- src/main/java/com/seplag/processoseletivo/application/usecases/autenticacao/impl/CadastraUsuarioUseCaseImpl.java
+++ src/main/java/com/seplag/processoseletivo/application/usecases/autenticacao/impl/CadastraUsuarioUseCaseImpl.java
@@ -3,11 +3,13 @@ package com.seplag.processoseletivo.application.usecases.autenticacao.impl;
 import com.seplag.processoseletivo.application.dto.autenticacao.UsuarioRequestDto;
 import com.seplag.processoseletivo.application.dto.shared.MensagemRetorno;
 import com.seplag.processoseletivo.application.usecases.autenticacao.CadastraUsuarioUseCase;
+import com.seplag.processoseletivo.domain.enums.Role;
 import com.seplag.processoseletivo.domain.model.Usuario;
 import com.seplag.processoseletivo.domain.repositories.UsuarioRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -30,6 +32,8 @@ public class CadastraUsuarioUseCaseImpl implements CadastraUsuarioUseCase {
                 usuarioRequestDto.email(),
                 senhaCriptografada
         );
+
+        usuario.setRoles(Set.of(Role.ADMIN));
 
         usuarioRepository.criar(usuario);
         return new MensagemRetorno("Usuário cadastrado com sucesso");
